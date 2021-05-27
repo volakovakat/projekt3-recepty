7ce200519dd3700745bf8378bc4818bab86cb6cc
kaczenka <75174671+kaczenka@users.noreply.github.com>
2021-05-27 16:41:26 +0200

    Rozbalení detailu vybraného receptu z nabídky

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ HTML vzor, jak vygenerovaný recept vypadá, je zakomentovaný v index.html. */
 let kategorie;
 let receptar = document.querySelector('.receptar');
 receptar.classList.add('receptar');
+let kontejner = document.querySelector('.kontejner');
 
 let hlavicka = document.querySelector('.hlavicka');
-
 
 function pridejReceptDoMenu(recept) {
     let divRecept = document.createElement('div');
     divRecept.classList.add('recept');
     receptar.appendChild(divRecept);
+    divRecept.onclick = function() {
+        precistRecept(recept.id);
+    };
     
     let divReceptObrazek = document.createElement('div');
     divReceptObrazek.classList.add('recept-obrazek');
@@ -102,18 +105,38 @@ function seraditPodleHodnoceni(select) {
             
         }
     }
-/*     kategorie = select.value;
-    if (recepty[i].kategorie === kategorie) {
-    pridejReceptDoMenu(recepty[i]);
-    recepty.kategorie.sort(function(recept1, recept2){return recept1.hodnoceni > recept2.hodnoceni});
-    hledatPodleKategorie();
-  } */
   seraditPodleHodnoceni();
 
 
 /* 5) Na recepty v seznamu by mělo jít kliknout a na pravé polovině, se objeví detail receptu.
 Doplň patričné údaje receptu do HTML prvků s ID recept-foto, recept-kategorie,
 recept-hodnoceni, recept-nazev, recept-popis. */
+function receptInfo(recept) {
+        let receptFoto = document.querySelector('#recept-foto');
+        receptFoto.src = recept.img;
+    
+        let receptIdKategorie = document.querySelector('#recept-kategorie');
+        receptIdKategorie.innerHTML = recept.kategorie;
+    
+        let receptIdHodnoceni = document.querySelector('#recept-hodnoceni');
+        receptIdHodnoceni.innerHTML = recept.hodnoceni;
+    
+        let receptNazev = document.querySelector('#recept-nazev');
+        receptNazev.innerHTML = recept.nadpis;
+    
+        let receptPopis = document.querySelector('#recept-popis');
+        receptPopis.innerHTML = recept.popis;
+};
+
+function precistRecept(id) {
+    for (let i = 0; i < recepty.length; i++) {
+        if (recepty[i].id === id) {
+            receptInfo(recepty[i]);
+        }
+    } 
+};
+
+precistRecept(0);
 
 /* 6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
 */
